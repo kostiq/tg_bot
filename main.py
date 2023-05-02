50601bfb2b86f142e0e53802ee345a26cd969f39
kkuzmin-mercaux <konstantin@mercaux.com>
2023-05-02 16:30:17 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import logging
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent, \
+    Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler, MessageHandler, filters
+
+TELEGRAM_TOKEN = ''
+CHATGPT_KEY = ''
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    buttons = []
+
+    for key, value in {"Name": "John", "Language": "Python", "API": "pyTelegramBotAPI"}.items():
+        buttons.append(
+            [InlineKeyboardButton(text=key, url='google.com')]
+        )
+    keyboard = InlineKeyboardMarkup(buttons)
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="A two-column menu", reply_markup=keyboard)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+    )
+
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+    cmd_handler = CommandHandler("hello", hello)
+    # echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    # caps_handler = CommandHandler('caps', caps)
+    inline_caps_handler = InlineQueryHandler(inline_caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
+    application.add_handler(cmd_handler)
+    # application.add_handler(echo_handler)
+    # application.add_handler(caps_handler)
+    application.add_handler(inline_caps_handler)
+    application.add_handler(unknown_handler)
+    application.add_handler(MessageHandler(filters.TEXT, menu))
+
+    application.run_polling()
+
+
